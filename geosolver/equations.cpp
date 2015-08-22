53da33133124b1590196660614db6f791dc31011
eagle2com <peter.lichard@gmail.com>
2015-08-22 11:49:40 +0200

    Expand was broken actualy, fixed.

--- geosolver/equations.cpp
+++ geosolver/equations.cpp
@@ -9,9 +9,10 @@ using std::endl;
 namespace geo {
 	namespace equ {
 
-		std::map<std::string, s32> name_id_map;
-		std::map<s32, std::string> id_name_map;
-		s32 id = 0;
+		std::map<std::string, s64> name_id_map;
+		std::map<s64, std::string> id_name_map;
+		s64 id = 0;
+		GEOSOLVER_API s32 SymNode::total_ctor_calls = 0;
 
 		GEOSOLVER_API void sym_kernel_init()
 		{
@@ -19,7 +20,7 @@ namespace geo {
 			name_id_map.clear();
 		}
 
-		GEOSOLVER_API s32 sym_kernel_get_id(std::string name)
+		GEOSOLVER_API s64 sym_kernel_get_id(std::string name)
 		{
 			auto it = name_id_map.find(name);
 			if (it != name_id_map.end())
@@ -35,7 +36,7 @@ namespace geo {
 			}
 		}
 
-		GEOSOLVER_API std::string sym_kernel_get_name(s32 id)
+		GEOSOLVER_API std::string sym_kernel_get_name(s64 id)
 		{
 			//cout << "sym_kernel_get_name: " << id << endl;
 			auto it = id_name_map.find(id);
--- geosolver/equations.hpp
+++ geosolver/equations.hpp
@@ -4,6 +4,10 @@
 #include <memory>
 #include <string>
 #include <list>
+#include <algorithm>
+
+using std::cout;
+using std::endl;
 
 namespace geo {
 	namespace equ {
@@ -14,50 +18,160 @@ namespace geo {
 		using SymNodeList = std::list<SymNode>;
 
 		GEOSOLVER_API void sym_kernel_init();
-		GEOSOLVER_API s32 sym_kernel_get_id(std::string name);
-		GEOSOLVER_API std::string sym_kernel_get_name(s32 id);
+		GEOSOLVER_API s64 sym_kernel_get_id(std::string name);
+		GEOSOLVER_API std::string sym_kernel_get_name(s64 id);
 
+		int simplify(SymNode &node);
+
+		enum class SYM_TYPE { OP_NONE, OP_ADD, OP_SUB, OP_MUL, OP_DIV, OP_VAL_RATIONAL, OP_VAL_REAL, OP_VAR };
 		struct SymNode
 		{
-			enum { OP_NONE, OP_ADD, OP_SUB, OP_MUL, /*OP_SQUARE, */ OP_DIV, OP_VAL_RATIONAL, OP_VAL_REAL, OP_VAR};
+			static GEOSOLVER_API s32 total_ctor_calls;
 			SymNodeList nodes;
-			int type;
-			void *data;
+			SYM_TYPE type;
+			union
+			{
+				r64 real_value;
+				struct
+				{
+					s32 id;
+					r32 power;
+				};
+			};
 
 			SymNode()
 			{
-				type = OP_NONE;
-				data = nullptr;
+				type = SYM_TYPE::OP_NONE;
+				total_ctor_calls++;
 			}
 
-			SymNode(int type): type(type), data(nullptr){}
+			SymNode(SYM_TYPE type): type(type) { total_ctor_calls++; }
 
-			SymNode(SymNode v1, SymNode v2, int type)
+			SymNode(SymNode v1, SymNode v2, SYM_TYPE type)
 			{
 				this->type = type;
 
-				if (v1.type && v2.type)
+#if 0
+				if (type == SYM_TYPE::OP_ADD)
 				{
-					if (type == v1.type)
+					if (v1.is_var() && v2.is_var() && v1.id == v2.id)
 					{
-						nodes.splice(nodes.end(), v1.nodes);
-					}
-					else
-					{
-						nodes.push_back(v1);
-					}
-
-					if (type == v2.type)
-					{
-						nodes.splice(nodes.end(), v2.nodes);
-					}
-					else
-					{
-						nodes.push_back(v2);
+						this->type = SYM_TYPE::OP_MUL;
+						nodes.p
+							nodes.push_back(v1);
 					}
 				}
 
-				data = nullptr;
+				else
+#endif // 0
+				if (type == SYM_TYPE::OP_MUL)
+				{
+					if (v1.is_real() && v1.real_value == 1)
+					{
+						this->type = v2.type;
+						real_value = v2.real_value;
+					}
+					else if (v2.is_real() && v2.real_value == 1)
+					{
+						this->type = v1.type;
+						real_value = v1.real_value;
+					}
+					else
+					{
+						if (!v1.is_none() && !v2.is_none())
+						{
+							if (v1.is_var() && v2.is_var() && v1.id == v2.id)
+							{
+								this->id = v1.id;
+								this->power = v1.power + v2.power;
+								this->type = SYM_TYPE::OP_VAR;
+							}	
+							else
+							{
+								if (type == v1.type)
+								{
+									nodes.splice(nodes.end(), v1.nodes);
+								}
+								else
+								{
+									nodes.push_back(v1);
+								}
+
+								if (type == v2.type)
+								{
+									nodes.splice(nodes.end(), v2.nodes);
+								}
+								else
+								{
+									nodes.push_back(v2);
+								}
+							}
+						}
+					}
+
+				}
+				else
+				{
+					if (!v1.is_none() && !v2.is_none())
+					{
+						if (v1.is_var() && v2.is_var() && v1.id == v2.id && type == SYM_TYPE::OP_MUL)
+						{
+							this->id = id;
+							power = v1.power + v2.power;
+						}
+						else if (type == v1.type)
+						{
+							nodes.splice(nodes.end(), v1.nodes);
+						}
+						else
+						{
+							nodes.push_back(v1);
+						}
+
+						if (type == v2.type)
+						{
+							nodes.splice(nodes.end(), v2.nodes);
+						}
+						else
+						{
+							nodes.push_back(v2);
+						}
+					}
+				}
+				
+				total_ctor_calls++;
+			}
+
+			
+
+			bool is_final() const
+			{
+				return nodes.empty();
+			}
+
+			bool is_real() const
+			{
+				return type == SYM_TYPE::OP_VAL_REAL;
+			}
+
+			bool is_add() const
+			{
+				return type == SYM_TYPE::OP_ADD;
+			}
+
+			bool is_mul() const
+			{
+				return type == SYM_TYPE::OP_MUL;
+			}
+
+			bool is_var() const
+			{
+				return type == SYM_TYPE::OP_VAR;
+			}
+
+			bool is_none() const
+			{
+				return type == SYM_TYPE::OP_NONE;
 			}
 		};
 
@@ -67,14 +181,20 @@ namespace geo {
 			auto it = node.nodes.begin();
 			switch (node.type)
 			{
-			case SymNode::OP_VAR:
-				out << sym_kernel_get_name(*(s32*)node.data) << " ";
+			case SYM_TYPE::OP_VAR:
+				out << sym_kernel_get_name(node.id);
+				if (node.power != 1)
+				{
+					out << "^" << node.power;
+				}
+				out << " ";
+
 				break;
-			case SymNode::OP_VAL_REAL:
-				out << *(r64*)node.data << " ";
+			case SYM_TYPE::OP_VAL_REAL:
+				out << node.real_value << " ";
 				break;
 			default:
-				if (!node.nodes.empty())
+				if (!node.nodes.empty() && node.is_add())
 					out << "( ";
 				for (; it != node.nodes.end() && i < node.nodes.size()-1; it++)
 				{
@@ -82,16 +202,16 @@ namespace geo {
 					
 					switch (node.type)
 					{
-					case SymNode::OP_ADD:
+					case SYM_TYPE::OP_ADD:
 						out << "+ ";
 						break;
-					case SymNode::OP_SUB:
+					case SYM_TYPE::OP_SUB:
 						out << "- ";
 						break;
-					case SymNode::OP_MUL:
+					case SYM_TYPE::OP_MUL:
 						out << "* ";
 						break;
-					case SymNode::OP_DIV:
+					case SYM_TYPE::OP_DIV:
 						out << "/ ";
 						break;
 					}
@@ -101,7 +221,8 @@ namespace geo {
 				if (!node.nodes.empty())
 				{
 					out << (*(it++));
-					out << ") ";
+					if(node.is_add())
+						out << ") ";
 				}
 				break;
 			}
@@ -121,39 +242,37 @@ namespace geo {
 
 		SymNode op_mul(SymNode lhs, SymNode rhs)
 		{
-			return SymNode(lhs, rhs, SymNode::OP_MUL);
+			return SymNode(lhs, rhs, SYM_TYPE::OP_MUL);
 		}
 
 		SymNode op_add(SymNode lhs, SymNode rhs)
 		{
-			return SymNode(lhs, rhs, SymNode::OP_ADD);
+			return SymNode(lhs, rhs, SYM_TYPE::OP_ADD);
 		}
 
 		SymNode op_sub(SymNode lhs, SymNode rhs)
 		{
-			return SymNode(lhs, rhs, SymNode::OP_SUB);
+			return SymNode(lhs, rhs, SYM_TYPE::OP_SUB);
 		}
 
 		SymNode op_div(SymNode lhs, SymNode rhs)
 		{
-			return SymNode(lhs, rhs, SymNode::OP_DIV);
+			return SymNode(lhs, rhs, SYM_TYPE::OP_DIV);
 		}
 
 		SymNode op_val_real(r64 value)
 		{
-			r64 *v = new r64{ value };
-			auto p = SymNode(SymNode::OP_VAL_REAL);
-			p.data = v;
+			auto p = SymNode(SYM_TYPE::OP_VAL_REAL);
+			p.real_value = value;
 			return p;
 		}
 
 		SymNode op_var(std::string&& name)
 		{
-			s32 *v = new s32(0);
-			*v = sym_kernel_get_id(name);
 			auto p = SymNode();
-			p.type = SymNode::OP_VAR;
-			p.data = v;
+			p.type = SYM_TYPE::OP_VAR;
+			p.power = 1;
+			p.id = sym_kernel_get_id(name);
 			return p;
 		}
 
@@ -267,41 +386,68 @@ namespace geo {
 
 		//-----------------------
 
-		SymNode expand_mul_2(SymNode node1, SymNode node2)
+		SymNode expand_mul_sub(SymNode n1, SymNode n2)
 		{
-			SymNode result(SymNode::OP_ADD);
-
-			SymNodeList::iterator it1;
-			SymNodeList::iterator it2;
-
-			if (node1.nodes.empty())
+			SymNode result;
+			
+			if (n1.is_final() && n2.is_final())
 			{
-				it2 = node2.nodes.begin();
-				while (it2 != node2.nodes.end())
+				result = n1*n2;
+			}
+			else if (n1.is_final())
+			{
+				if (n2.is_add())
 				{
-					result = result + node1*(*it2);
-					it2++;
+					auto it = n2.nodes.begin();
+					result = n1*(*it);
+					result.type = SYM_TYPE::OP_ADD;
+					while (it != n2.nodes.end())
+					{
+						result = result + n1*(*it);
+						it++;
+					}
+				}
+				else if (n2.is_mul())
+				{
+					result = n1*n2;
+				}
+			}
+			else if (n2.is_final())
+			{
+				if (n1.is_add())
+				{
+					auto it = n1.nodes.begin();
+					result = n2*(*it);
+					it++;
+					while (it != n1.nodes.end())
+					{
+						result = result + n2*(*it);
+						it++;
+					}
+				}
+				else if (n1.is_mul())
+				{
+					result = n1*n2;
 				}
 			}
 			else
 			{
-				it1 = node1.nodes.begin();
-				while(it1 != node1.nodes.end())
+				if (n1.is_add() && n2.is_add())
 				{
-					if (node2.nodes.empty())
+					for (auto i1 : n1.nodes)
 					{
-						result = result + node2*(*it1);
-					}
-					else
-					{
-						it2 = node2.nodes.begin();
-						while (it2 != node2.nodes.end())
+						for (auto i2 : n2.nodes)
 						{
-							result = result + (*it1)*(*it2);
-							it2++;
+							if (result.is_none())
+							{
+								result = i1*i2;
+							}
+							else
+							{
+								result = result + i1*i2;
+							}
 						}
 					}
-					it1++;
 				}
 			}
 
@@ -310,16 +456,13 @@ namespace geo {
 
 		SymNode expand_mul(SymNode node)
 		{
-			SymNodeList::iterator it1 = node.nodes.begin();
-			SymNodeList::iterator it2 = it1;
-			it2++;
-			
-			SymNode result = expand_mul_2(*it1, *it2);
-			it2++;
-			while (it2 != node.nodes.end())
+			auto it = node.nodes.begin();
+			SymNode result = *it;
+			it++;
+			while (it != node.nodes.end())
 			{
-				result = expand_mul_2(result, *it2);
-				it2++;
+				result = expand_mul_sub(result, *it);
+				it++;
 			}
 
 			return result;
@@ -327,22 +470,26 @@ namespace geo {
 
 		SymNode expand(SymNode node)
 		{
+			//cout << "expanding [ " << node << "]"<<endl;
 			SymNode result;
 			if (!node.nodes.empty())
 			{
 				switch (node.type)
 				{
-				case SymNode::OP_MUL:
+				case SYM_TYPE::OP_MUL:
 					result = expand_mul(node);
 					break;
-				case SymNode::OP_DIV:
+				case SYM_TYPE::OP_DIV:
 					break;
-				case SymNode::OP_ADD:
-					result = SymNode(SymNode::OP_ADD);
+				case SYM_TYPE::OP_ADD:
+					result = SymNode(SYM_TYPE::OP_ADD);
 					for (auto it : node.nodes)
 					{
 						result = result + expand(it);
 					}
+					break;
+				case SYM_TYPE::OP_VAR:
+					result = node;
 					break;
 				}
 				return result;
@@ -351,36 +498,141 @@ namespace geo {
 				return node;
 		}
 
-		bool node_sort_pred(SymNode& lhs, SymNode& rhs)
+		bool node_sort_pred(SymNode lhs, SymNode rhs)
 		{
 			if (lhs.type == rhs.type)
 			{
-				if (lhs.type == SymNode::OP_VAR)
+				if (lhs.type == SYM_TYPE::OP_VAR)
 				{
-					return *(s32*)lhs.data < *(s32*)rhs.data;
+					if (lhs.power == rhs.power)
+					{
+						return lhs.id < rhs.id;
+					}
+					else
+					{
+						return lhs.power > rhs.power;
+					}
 				}
-				else if (lhs.type == SymNode::OP_VAL_REAL)
+				else if (lhs.type == SYM_TYPE::OP_VAL_REAL)
 				{
-					return *(r64*)lhs.data < *(r64*)rhs.data;
+					return lhs.real_value > lhs.real_value;
+				}
+				else if (lhs.is_add())
+				{
+					return !node_sort_pred(lhs.nodes.front(), rhs.nodes.front());
 				}
 				else
 					return false;
 			}
 			else
-				return lhs.type < rhs.type;
+				return lhs.type > rhs.type;
 		}
 
-		void simplify(SymNode &node)
+		int simplify(SymNode &node)
 		{
+			int n_op = 0;
 			if (!node.nodes.empty())
 			{
-				node.nodes.sort(node_sort_pred);
-
-				for (auto& n : node.nodes)
+				if (!std::is_sorted(node.nodes.begin(), node.nodes.end(), node_sort_pred))
 				{
-					simplify(n);
+					node.nodes.sort(node_sort_pred);
+					n_op++;
 				}
+				
+				for (auto &n : node.nodes)
+				{
+					n_op += simplify(n);
+				}
+
+				auto it1 = node.nodes.begin();
+				auto it2 = it1;
+				it2++;
+
+				while (it2 != node.nodes.end())
+				{
+					if (it1->type == it2->type && it1->type == SYM_TYPE::OP_VAL_REAL && node.type == SYM_TYPE::OP_ADD)
+					{
+						it1->real_value += it2->real_value;
+						node.nodes.erase(it2++);
+						n_op++;
+						if (it2 == node.nodes.end())
+							break;
+					}
+					else if (it1->type == it2->type && it1->type == SYM_TYPE::OP_VAL_REAL && node.type == SYM_TYPE::OP_MUL)
+					{
+						it1->real_value *= it2->real_value;
+						node.nodes.erase(it2++);
+						n_op++;
+						if (it2 == node.nodes.end())
+							break;
+					}
+					else
+					{
+						it1++;
+						if (it1 != node.nodes.end())
+						{
+							it2 = it1;
+							it2++;
+							if (it2 == node.nodes.end())
+								break;
+						}
+					}
+				}
+
+				if (node.nodes.size() == 1)
+				{
+					node.type = node.nodes.front().type;
+					if (node.type == SYM_TYPE::OP_VAR)
+					{
+						node.id = node.nodes.front().id;
+						n_op++;
+					}
+					else if (node.type == SYM_TYPE::OP_VAL_REAL)
+					{
+						node.real_value = node.nodes.front().real_value;
+						n_op++;
+					}
+
+					node.nodes.clear();
+				}
+
+#if 1
+				if (node.is_mul() && node.nodes.front().is_var())
+				{
+					auto first_it = node.nodes.begin();
+					auto last_it = first_it;
+
+					while (first_it != node.nodes.end())
+					{
+						r32 power = 1;
+						last_it++;
+						if (last_it != node.nodes.end() && last_it->id == first_it->id)
+						{
+							while (last_it != node.nodes.end() && last_it->id == first_it->id)
+							{
+								power += last_it->power;
+								last_it++;
+							}
+
+							SymNode power_node;
+							power_node.power = power;
+							power_node.type = SYM_TYPE::OP_VAR;
+							power_node.id = first_it->id;
+
+							node.nodes.erase(first_it, last_it);
+							node.nodes.push_front(power_node);
+						}
+
+						first_it = last_it;
+					}
+				}
+#endif // 0
 			}
+
+			if (n_op > 0)
+				simplify(node);
+
+			return n_op;
 		}
 
 		struct Equation
@@ -388,16 +640,5 @@ namespace geo {
 			SymNode lhs;
 			SymNode rhs;
 		};
-
-		Equation solve(Equation equ, SymNodePtr var)
-		{
-			if (var->type != SymNode::OP_VAR)
-			{
-				return Equation{ 0,0 };
-			}
-			//TODO: Expand this to a list of variables
-			// Find the highest degree of the var
-			return Equation{ 0,0 };
-		}
 	}
 }
